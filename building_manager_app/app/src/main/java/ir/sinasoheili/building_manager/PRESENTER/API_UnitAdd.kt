3c97be11e7d167d0f92aac5c420bf334389f55ed
SINA <sinasoheili79@gmail.com>
2020-09-29 22:20:34 +0330

    create class to handle add unit to server

---
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/API_UnitAdd.kt
@@ -0,0 +1,51 @@
+package ir.sinasoheili.building_manager.PRESENTER
+
+import android.content.Context
+import android.widget.Toast
+import ir.sinasoheili.building_manager.MODEL.Unit
+import ir.sinasoheili.building_manager.MODEL.UnitAddResponse
+import ir.sinasoheili.building_manager.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class API_UnitAdd constructor(context:Context)
+{
+    private val context:Context = context
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(APIs.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    private val api:APIs = retrofit.create(APIs::class.java)
+
+    fun start(unit:Unit)
+    {
+        var call : Call<UnitAddResponse> = api.unitAdd(unit.owner_name , unit.phone , unit.unit_number , unit.tag , unit.building_id)
+        call.enqueue(object: Callback<UnitAddResponse>
+        {
+            override fun onFailure(call: Call<UnitAddResponse>, t: Throwable)
+            {
+                Toast.makeText(context , context.getString(R.string.toast_fail_connect_to_server) , Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<UnitAddResponse> , response: Response<UnitAddResponse>)
+            {
+                if(response.code() == 200)
+                {
+                    if(response.body()!!.status == true)
+                    {
+                        Toast.makeText(context , context.getString(R.string.toast_success_register), Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(context , context.getString(R.string.toast_unsuccess_register), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+    }
+}
