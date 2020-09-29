63bdd81ad675912fd6b88f70f92cc540607b7c4d
SINA <sinasoheili79@gmail.com>
2020-09-29 21:55:51 +0330

    create class to handle get list of building

---
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/BuildingListHandlerAPI.kt
@@ -0,0 +1,48 @@
+package ir.sinasoheili.building_manager.PRESENTER
+
+import android.content.Context
+import android.widget.Toast
+import ir.sinasoheili.building_manager.MODEL.Building
+import ir.sinasoheili.building_manager.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class BuildingListHandlerAPI constructor(context: Context)
+{
+    private val context : Context = context
+
+    private val retrofit : Retrofit = Retrofit.Builder()
+            .baseUrl(APIs.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    private val api : APIs = retrofit.create(APIs::class.java)
+
+
+    fun start(managerId:Int):List<Building>?
+    {
+        var buildingList : List<Building>? = null
+
+        val call : Call<List<Building>> = api.buildingList(managerId)
+        call.enqueue(object: Callback<List<Building>>
+        {
+            override fun onFailure(call: Call<List<Building>>, t: Throwable)
+            {
+                Toast.makeText(context , context.getString(R.string.toast_fail_connect_to_server) , Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<List<Building>> , response: Response<List<Building>>)
+            {
+                if(response.code() == 200)
+                {
+                    buildingList = response.body()
+                }
+            }
+        })
+
+        return buildingList
+    }
+}
