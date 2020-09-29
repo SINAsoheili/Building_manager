ab557e8f313af46866e159bb5ba1af071309c83a
SINA <sinasoheili79@gmail.com>
2020-09-29 17:23:51 +0330

    api interface of add unit

---
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/UnitAddResponse.kt
@@ -0,0 +1,3 @@
+package ir.sinasoheili.building_manager.MODEL
+
+data class UnitAddResponse constructor(val status:Boolean)
--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/APIs.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/APIs.kt
@@ -3,6 +3,7 @@ package ir.sinasoheili.building_manager.PRESENTER
 import ir.sinasoheili.building_manager.MODEL.Building
 import ir.sinasoheili.building_manager.MODEL.BuildingRegisterResponse
 import ir.sinasoheili.building_manager.MODEL.ManagerRegisterResponse
+import ir.sinasoheili.building_manager.MODEL.UnitAddResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -17,4 +18,7 @@ interface APIs
 
     @GET("/building/list")
     fun buildingList(@Query("manager_id") manager_id:Int):Call<List<Building>>
+
+    @GET("/unit/add")
+    fun unitAdd(@Query("owner_name") owner_name:String , @Query("phone") phone:String , @Query("unit_number") unit_number:String , @Query("tag") tag:String , @Query("building_id") building_id:String):Call<UnitAddResponse>
 }
