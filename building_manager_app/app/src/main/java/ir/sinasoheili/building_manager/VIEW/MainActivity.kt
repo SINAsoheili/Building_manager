4fc2f5bc29dadb0b1e15428b736c800a329ccc43
SINA <sinasoheili79@gmail.com>
2020-09-30 18:28:03 +0330

    design main page to select user role and show register page

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/MainActivity.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/MainActivity.kt
@@ -2,12 +2,53 @@ package ir.sinasoheili.building_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.Toast
+import ir.sinasoheili.building_manager.VIEW.FragmentSetRoleManager
+import ir.sinasoheili.building_manager.VIEW.FragmentSetRoleUser
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() , View.OnClickListener
 {
+    private var btn_manager:Button? = null
+    private var btn_user:Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initObj()
+    }
+
+    private fun initObj()
+    {
+        btn_manager = findViewById(R.id.btn_setRole_manager)
+        btn_manager?.setOnClickListener(this)
+
+        btn_user = findViewById(R.id.btn_setRole_user)
+        btn_user?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?)
+    {
+        when(view)
+        {
+            btn_manager->
+            {
+                val managerFragment:FragmentSetRoleManager = FragmentSetRoleManager()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_setRole , managerFragment).addToBackStack(null).commit()
+            }
+
+            btn_user->
+            {
+                val userFragment:FragmentSetRoleUser = FragmentSetRoleUser()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_setRole , userFragment).addToBackStack(null).commit()
+            }
+        }
     }
 }
