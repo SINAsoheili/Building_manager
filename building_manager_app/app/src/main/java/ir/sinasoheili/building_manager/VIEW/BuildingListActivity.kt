07785b83928e66d2699ff72731a48060b167e4c8
SINA <sinasoheili79@gmail.com>
2020-10-01 16:38:10 +0330

    temp activity to show building list related to manageer

---
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/BuildingListActivity.kt
@@ -0,0 +1,12 @@
+package ir.sinasoheili.building_manager.VIEW
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.sinasoheili.building_manager.R
+
+class BuildingListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_building_list)
+    }
+}
