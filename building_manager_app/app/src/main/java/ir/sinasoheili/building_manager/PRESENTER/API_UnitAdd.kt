31064f6fed2920189b6055075f85996407f60ed5
SINA <sinasoheili79@gmail.com>
2020-10-06 19:45:56 +0330

    change return data type to callback

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/API_UnitAdd.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/PRESENTER/API_UnitAdd.kt
@@ -22,27 +22,30 @@ class API_UnitAdd constructor(context:Context)
 
     private val api:APIs = retrofit.create(APIs::class.java)
 
-    fun start(unit:Unit):Boolean
+    fun start(unit:Unit , callBack:CallBack)
     {
-        var result:Boolean = false
-
         var call : Call<UnitAddResponse> = api.unitAdd(unit.owner_name , unit.phone , unit.unit_number , unit.tag , unit.building_id)
         call.enqueue(object: Callback<UnitAddResponse>
         {
             override fun onFailure(call: Call<UnitAddResponse>, t: Throwable)
             {
-                Toast.makeText(context , context.getString(R.string.toast_fail_connect_to_server) , Toast.LENGTH_SHORT).show()
+                callBack.onFailure()
+//                Toast.makeText(context , context.getString(R.string.toast_fail_connect_to_server) , Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<UnitAddResponse> , response: Response<UnitAddResponse>)
             {
                 if(response.code() == 200)
                 {
-                    result = response.body()!!.status
+                    callBack.onResponse(response.body()!!)
                 }
             }
         })
+    }
 
-        return result
+    interface CallBack
+    {
+        fun onFailure()
+        fun onResponse(response:UnitAddResponse)
     }
 }
