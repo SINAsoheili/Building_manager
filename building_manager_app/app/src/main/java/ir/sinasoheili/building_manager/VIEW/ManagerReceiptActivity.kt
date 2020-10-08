3126f8f7de89cf374080f61d770ce5e0f21a66ef
SINA <sinasoheili79@gmail.com>
2020-10-08 11:21:29 +0330

    show register_receipt_fragment

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerReceiptActivity.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerReceiptActivity.kt
@@ -2,13 +2,37 @@ package ir.sinasoheili.building_manager.VIEW
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ir.sinasoheili.building_manager.R
 
-class ManagerReceiptActivity : AppCompatActivity()
+class ManagerReceiptActivity : AppCompatActivity(), View.OnClickListener
 {
+    private var fabRegisterReceipt : FloatingActionButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manager_receipt)
+
+        initObj()
+    }
+
+    private fun initObj()
+    {
+        fabRegisterReceipt = findViewById(R.id.fab_managerReceipt_AddReceipt)
+        fabRegisterReceipt!!.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?)
+    {
+        when(view)
+        {
+            fabRegisterReceipt ->
+            {
+                val fragment : FragmentRegisterNewReceipt = FragmentRegisterNewReceipt()
+                supportFragmentManager.beginTransaction().add(R.id.fl_managerReceipt , fragment).addToBackStack(null).commit()
+            }
+        }
     }
 }
