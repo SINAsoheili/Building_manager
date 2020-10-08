455987d790a3619611c1fdbe8a1fd22219186604
SINA <sinasoheili79@gmail.com>
2020-10-08 19:26:44 +0330

    show repair fragment

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerRepairActivity.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerRepairActivity.kt
@@ -1,14 +1,40 @@
 package ir.sinasoheili.building_manager.VIEW
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ir.sinasoheili.building_manager.R
 
-class ManagerRepairActivity : AppCompatActivity()
+class ManagerRepairActivity : AppCompatActivity(), View.OnClickListener
 {
+    private var fabAddRepair : FloatingActionButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manager_repair)
+
+        initObj()
+    }
+
+    private fun initObj()
+    {
+        fabAddRepair = findViewById(R.id.fab_managerRepair_addRepair)
+        fabAddRepair!!.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?)
+    {
+        when(view)
+        {
+            fabAddRepair->
+            {
+                val fragmentRepair : FragmentRegisterNewRepair = FragmentRegisterNewRepair()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_managerRepair , fragmentRepair).addToBackStack(null).commit()
+            }
+        }
     }
 }
