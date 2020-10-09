4264339dc5f2670d62da2b2833ffb1cc048b1aee
SINA <sinasoheili79@gmail.com>
2020-10-09 17:24:35 +0330

    add static function to convert int to ReceiptType

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/ReceiptType.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/ReceiptType.kt
@@ -6,6 +6,19 @@ enum class ReceiptType constructor(val persianName : String)
     water ("آب"),
     power ("برق");
 
+    companion object
+    {
+        fun getReceipt(item:Int):ReceiptType
+        {
+            when(item)
+            {
+                0 -> return ReceiptType.gas
+                1 -> return ReceiptType.water
+                else -> return ReceiptType.power
+            }
+        }
+    }
+
     override fun toString():String
     {
         return persianName
