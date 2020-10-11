ce0b6472abc00225ca731efd966d1f2e4aa315e7
SINA <sinasoheili79@gmail.com>
2020-10-11 22:12:07 +0330

    change in order of paid and unpaid

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/ChargeStatus.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/ChargeStatus.kt
@@ -2,8 +2,8 @@ package ir.sinasoheili.building_manager.MODEL
 
 enum class ChargeStatus constructor(val text:String)
 {
-    paid("پرداخت شده") ,
-    unpaid("پرداخت نشده");
+    unpaid("پرداخت نشده"),
+    paid("پرداخت شده") ;
 
     override fun toString(): String
     {
