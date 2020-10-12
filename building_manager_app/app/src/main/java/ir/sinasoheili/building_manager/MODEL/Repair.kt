a8fe40bd679589ad9768f4ae334026646ec16d52
SINA <sinasoheili79@gmail.com>
2020-10-12 18:15:23 +0330

    remove redundant code from Repair.kt

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Repair.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Repair.kt
@@ -4,8 +4,10 @@ import android.os.Build
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-class Repair constructor (date:String , comment:String , title:String , amount:Double , building_id:Int)
+class Repair constructor (val date:String , val comment:String , val title:String , val amount:Double , val building_id:Int)
 {
+    var id : Int = -1
+
     companion object
     {
         fun convertDate(date:String):String
@@ -20,48 +22,6 @@ class Repair constructor (date:String , comment:String , title:String , amount:D
 
             return DateStr
         }
-    }
-
-    var id : Int = -1
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var date : String = date
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var comment : String = comment
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var title : String = title
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var amount : Double = amount
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var building_id : Int = building_id
-    get() = field
-    set(value)
-    {
-        field = value
     }
 
     override fun toString():String
