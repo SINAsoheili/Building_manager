9917a08c7c28020845b3bf078dc075266ddbe334
SINA <sinasoheili79@gmail.com>
2020-10-12 18:19:21 +0330

    remove redundant code

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Unit.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Unit.kt
@@ -2,43 +2,8 @@ package ir.sinasoheili.building_manager.MODEL
 
 import java.io.Serializable
 
-class Unit constructor(owner_name:String , phone:String , unit_number:Int , tag:Int , building_id:Int) : Serializable
+class Unit constructor(val owner_name:String , val phone:String , val unit_number:Int , val tag:Int , val building_id:Int) : Serializable
 {
-    var owner_name : String = owner_name
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var phone : String = phone
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var unit_number : Int = unit_number
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var tag : Int =  tag
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var building_id : Int = building_id
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
     override fun toString():String
     {
         return "owner_name:$owner_name | phone:$phone | unit_number:$unit_number | tag:$tag | bulding_id:$building_id"
