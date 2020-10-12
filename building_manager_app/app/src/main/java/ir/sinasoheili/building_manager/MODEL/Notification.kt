8b7bdba1e12fe0d403919cce7b5ca614f92f9ff1
SINA <sinasoheili79@gmail.com>
2020-10-12 18:08:39 +0330

    remove redundant code from Notification.kt

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Notification.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Notification.kt
@@ -1,45 +1,13 @@
 package ir.sinasoheili.building_manager.MODEL
 
-class Notification constructor(text:String , title:String , building_id:Int)
+class Notification constructor(val text:String , val title:String , val building_id:Int)
 {
+    var id : Int = -1
+    var date : String? = null
+
     constructor(text:String , date:String , title:String , building_id:Int) : this(text , title , building_id)
     {
         this.date = date
-    }
-
-    var id : Int = -1
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var text : String = text
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var date : String? = null
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var title : String = title
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var building_id : Int = building_id
-    get() = building_id
-    set(value)
-    {
-        field = value
     }
 
     override fun toString():String
