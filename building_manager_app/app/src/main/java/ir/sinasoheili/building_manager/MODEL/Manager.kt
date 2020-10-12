868a1976d612d7f974fed42b3d743713dfbd4470
SINA <sinasoheili79@gmail.com>
2020-10-12 18:06:08 +0330

    remove redundant code from Manager.kt

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Manager.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Manager.kt
@@ -1,21 +1,7 @@
 package ir.sinasoheili.building_manager.MODEL
 
-class Manager constructor(id : Int , phone : String)
+class Manager constructor(val id : Int , val phone : String)
 {
-    var id : Int = id
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
-    var phone : String = phone
-    get() = field
-    set(value)
-    {
-        field = value
-    }
-
     override fun toString():String
     {
         return "id:$id | phone:$phone"
