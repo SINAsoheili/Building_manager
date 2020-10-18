3792bbc22f28e241ee8f186f260282b224cd0048
SINA <sinasoheili79@gmail.com>
2020-10-18 18:51:38 +0330

    create activity to show user dashboard when set role is user

---
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/UserDashboardActivity.kt
@@ -0,0 +1,14 @@
+package ir.sinasoheili.building_manager.VIEW
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.sinasoheili.building_manager.R
+
+class UserDashboardActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_dashboard)
+    }
+}
