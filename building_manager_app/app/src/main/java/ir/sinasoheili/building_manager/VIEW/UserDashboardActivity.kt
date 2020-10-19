d073c71f60e5e49b8b6a8dd8c6b7ea9512972328
SINA <sinasoheili79@gmail.com>
2020-10-19 16:34:52 +0330

    set id to menu of bottom navigation view and set select item

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/UserDashboardActivity.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/UserDashboardActivity.kt
@@ -2,19 +2,22 @@ package ir.sinasoheili.building_manager.VIEW
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ir.sinasoheili.building_manager.MODEL.Unit
 import ir.sinasoheili.building_manager.R
 import ir.sinasoheili.building_manager.PRESENTER.ContractUserDashboard.ContractUserDashboardView
 import ir.sinasoheili.building_manager.PRESENTER.PresenterUserDashboard
 
-class UserDashboardActivity : AppCompatActivity() , ContractUserDashboardView
+class UserDashboardActivity : AppCompatActivity() , ContractUserDashboardView, BottomNavigationView.OnNavigationItemSelectedListener
 {
     private var tvOwnerName : TextView? = null
     private var tvPhone : TextView? = null
     private var tvUnitNumber : TextView? = null
     private var tvTag : TextView? = null
+    private var bnv : BottomNavigationView? = null
 
     private var presenter : PresenterUserDashboard? = null
 
@@ -30,6 +33,9 @@ class UserDashboardActivity : AppCompatActivity() , ContractUserDashboardView
     private fun initObj()
     {
         presenter = PresenterUserDashboard(this@UserDashboardActivity , this)
+
+        bnv = findViewById(R.id.bnv_userDashboard)
+        bnv!!.setOnNavigationItemSelectedListener(this)
 
         tvOwnerName = findViewById(R.id.tv_userDashboard_ownerName)
         tvPhone = findViewById(R.id.tv_userDashboard_phone)
@@ -48,5 +54,44 @@ class UserDashboardActivity : AppCompatActivity() , ContractUserDashboardView
         tvPhone!!.text = unit.phone
         tvUnitNumber!!.text = unit.unit_number.toString()
         tvTag!!.text = unit.tag.toString()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean
+    {
+        when(item.itemId)
+        {
+            R.id.bnv_item_receipt ->
+            {
+                Toast.makeText(this@UserDashboardActivity , "receipt" , Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.bnv_item_repair ->
+            {
+                Toast.makeText(this@UserDashboardActivity , "repair" , Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.bnv_item_profile ->
+            {
+                Toast.makeText(this@UserDashboardActivity , "profile" , Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.bnv_item_notification ->
+            {
+                Toast.makeText(this@UserDashboardActivity , "notification" , Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.bnv_item_charge ->
+            {
+                Toast.makeText(this@UserDashboardActivity , "charge" , Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+        }
+
+        return false
     }
 }
