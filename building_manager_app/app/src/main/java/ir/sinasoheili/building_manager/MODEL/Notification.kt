f27599d09fd2dc138df6454defa762024a5147d2
SINA <sinasoheili79@gmail.com>
2020-10-27 18:52:48 +0330

    add convertDate function to notification model class

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Notification.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/MODEL/Notification.kt
@@ -1,9 +1,29 @@
 package ir.sinasoheili.building_manager.MODEL
 
+import android.os.Build
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 class Notification constructor(val text:String , val title:String , val building_id:Int)
 {
     var id : Int = -1
     var date : String? = null
+
+    companion object
+    {
+        fun convertDate(date:String):String
+        {
+            val DateStr : String = "${date.split(" ").get(1)} ${date.split(" ").get(2)} ${date.split(" ").get(3)}"
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            {
+                val format : DateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
+                var localDat : LocalDate = LocalDate.parse(DateStr , format)
+                return localDat.toString().replace("-" , "/")
+            }
+
+            return DateStr
+        }
+    }
 
     constructor(text:String , date:String , title:String , building_id:Int) : this(text , title , building_id)
     {
