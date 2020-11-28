110710b52630feafa591b9b7568791037887c1e6
SINA <sinasoheili79@gmail.com>
2020-11-28 21:40:58 +0330

    set progress bar and refresh button in notification activity

--- building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerNotificationActivity.kt
+++ building_manager_app/app/src/main/java/ir/sinasoheili/building_manager/VIEW/ManagerNotificationActivity.kt
@@ -17,6 +17,7 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
     private var listView : ListView? = null
     private var ivRefresh : ImageView? = null
     private var tvEmptyListAlert : TextView? = null
+    private var progressBar : ProgressBar? = null
 
     private var buildingId : Int = -1
     private var presenter : PresenterManagerNotification? = null
@@ -49,6 +50,8 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
         ivRefresh = findViewById(R.id.iv_managerNotification_refresh)
         ivRefresh!!.setOnClickListener(this)
 
+        progressBar = findViewById(R.id.pb_managerNotification_progressBar)
+
         tvEmptyListAlert = findViewById(R.id.tv_managerNotification_emptyList)
     }
 
@@ -76,6 +79,9 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
 
             ivRefresh ->
             {
+                invisibleRefreshButton()
+                visibleProgressBar()
+                invisibleListView()
                 presenter!!.fetchNotificationList(buildingId)
             }
         }
@@ -97,6 +103,10 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
 
     override fun showToast(text: String)
     {
+        invisibleProgressBar()
+        visibleRefreshButton()
+        invisibleListView()
+
         Toast.makeText(this@ManagerNotificationActivity , text , Toast.LENGTH_SHORT).show()
     }
 
@@ -105,6 +115,7 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
         invisibleRefreshButton()
         invisibleTextViewEmptyListAlert()
         visibleListView()
+        invisibleProgressBar()
 
         val adapter : ManagerNotificationListAdapter = ManagerNotificationListAdapter(this@ManagerNotificationActivity , items)
         listView!!.adapter = adapter
@@ -159,5 +170,15 @@ class ManagerNotificationActivity : AppCompatActivity() , ContractManagerNotific
     private fun invisibleTextViewEmptyListAlert()
     {
         tvEmptyListAlert!!.visibility = View.GONE
+    }
+
+    private fun visibleProgressBar()
+    {
+        progressBar!!.visibility = View.VISIBLE
+    }
+
+    private fun invisibleProgressBar()
+    {
+        progressBar!!.visibility = View.GONE
     }
 }
